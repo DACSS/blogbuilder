c876df1167f39e10fe849c305a00ce387f2d462f
Hans <hansquiogue@gmail.com>
2021-04-11 17:26:46 -0400

    Add ability to dynamically update YAML headers

--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,10 @@
-#' @importFrom usethis create_from_github
+# Creates a new R project from a GitHub repo
 create_dacss_proj <- function(repo_link, directory) {
   if (length(directory) == 0L) {
     stop('Directory is invalid.')
   # Creates new R project based on user repo
   } else {
-    create_from_github(
+    usethis::create_from_github(
       repo_link,
       destdir = directory,
       fork = FALSE,
@@ -14,10 +14,46 @@ create_dacss_proj <- function(repo_link, directory) {
   }
 }
 
-#' @importFrom usethis create_from_github
+# Allows user input in selecting a directory
 directory_input <- function() {
   cat('Please choose a directory you would like to store the new R project.\n\n')
   Sys.sleep(1)
-  directory <- choose_dir()
+  directory <- easycsv::choose_dir()
   return(directory)
+}
+
+# Updates YAML headers programmatically
+# Original code by @r2evans: https://stackoverflow.com/users/3358272/r2evans
+change_yaml_matter <- function(input_file, ..., output_file) {
+  input_lines <- readLines(input_file)
+  delimiters <- grep("^---\\s*$", input_lines)
+  if (!length(delimiters)) {
+    stop("unable to find yaml delimiters")
+  } else if (length(delimiters) == 1L) {
+    if (delimiters[1] == 1L) {
+      stop("cannot find second delimiter, first is on line 1")
+    } else {
+      # found just one set, assume it is *closing* the yaml matter;
+      # fake a preceding line of delimiter
+      delimiters <- c(0L, delimiters[1])
+    }
+  }
+  delimiters <- delimiters[1:2]
+  yaml_list <- yaml::yaml.load(input_lines[(delimiters[1]+1):(delimiters[2]-1)])
+
+  dots <- list(...)
+  yaml_list <- c(yaml_list[setdiff(names(yaml_list), names(dots))], dots)
+
+  output_lines <- c(
+    if (delimiters[1] > 0) input_lines[1:(delimiters[1])],
+    strsplit(yaml::as.yaml(yaml_list), "\n")[[1]],
+    input_lines[-(1:(delimiters[2]-1))]
+  )
+
+  if (missing(output_file)) {
+    return(output_lines)
+  } else {
+    writeLines(output_lines, con = output_file)
+    return(invisible(output_lines))
+  }
 }
