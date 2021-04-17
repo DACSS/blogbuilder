cebcb20cb3e4c3dd222762b5ed173874d6251974
Hans <hansquiogue@gmail.com>
2021-04-16 21:11:35 -0400

    Additional update functions

--- R/update.R
+++ R/update.R
@@ -17,9 +17,29 @@ update_course_semester <- function(semester) {
   update_env_template('SEMESTER', semester)
 }
 
-update_instructor_name <- function(name) {
+# TODO: Documentation and export
+update_instructor <- function(name, prof_pic = "NA") {
+  # Update instructor name
   update_env_template('INSTRUCTOR_NAME', name)
   change_yaml_matter('instructor.Rmd', title = name, output_file = 'instructor.Rmd')
+
+  # Update instructor profile picture
+  if (prof_pic != "NA") {
+    update_env_template('INSTRUCTOR_PROF_PIC', prof_pic)
+    base_url <- 'https://drive.google.com/uc?export=view&id='
+    picture <- paste(base_url, get_drive_id(prof_pic), sep = '')
+    change_yaml_matter('instructor.Rmd', image = picture, output_file = 'instructor.Rmd')
+  }
+}
+
+# TODO: Documentation and export
+update_repo_link <- function(repo) {
+  link <- get_github_pages_link(repo)
+  change_yaml_matter('_site.yml', base_url = repo, output_file = '_site.yml')
+  change_yaml_matter('_site.yml',
+                     navbar = list(logo = list
+                                  (image = 'images/UMass White Workmark Horiz.png',
+                                   href = link)))
 }
 
 # Updates file
