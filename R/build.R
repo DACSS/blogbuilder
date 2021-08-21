efbddc4150f1dc1afb09157ac3f08559e270a987
Naman Ranawat <namanranawat159@gmail.com>
2021-08-21 15:43:46 +0530

    Added build_latest_posts()

--- R/build.R
+++ R/build.R
@@ -30,6 +30,36 @@ build_all_posts <- function() {
 }
 
 
+#' Build latest blog posts
+#'
+#' Render the posts added or modified in the most recent git pull.
+#' Recommended use is only after git pull. In other cases and for a foolproof rendering of posts, consider using build_all_posts().
+#'
+#' @export
+build_lates_posts <- function() {
+  correct_env()
+
+  file_names <- list.files('_posts', '*.Rmd', recursive = TRUE, full.names=TRUE) # Find file paths/names
+  df <- file.info(file_names)   # Get info for the file paths - we want mtime from this ie. last modified
+  max_date <- max(df$mtime)     # We find the last modified post and take it as the standard timestamp for the latest posts
+
+  for (row in 1:nrow(df)) {     # Loop through the dataframe
+
+    # message(paste(file_names[row], difftime(max_date, df[[row, 'mtime']])))
+
+    if (difftime(max_date, df[[row, 'mtime']]) < 1) {     # Even the latest modified files have a difference of 0.0001 seconds at least so we take all the files which have time stamps less than 1 second away from the latest modified file's timestamp
+      # similar to build_all_posts() - renders all posts whose file paths we provide
+      post <- file_names[row]
+      change_yaml_matter(post, draft = FALSE, output_file = post)
+      rmarkdown::render(post, quiet = TRUE)
+      message(paste(post, 'rendered.'))
+      change_yaml_matter(post, draft = TRUE, output_file = post)
+    }
+  }
+  preview_site('Success! Posts rendered successfully.')
+}
+
+
 #' Builds all student pages
 #'
 #' Builds all student pages for a DACSS course site.
